408bcdb975c00a5b9a242b7d17a32d468095187f
Laurascardozo <143517864+Laurascardozo@users.noreply.github.com>
2023-11-08 03:01:45 +0000

    Counter created

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,19 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+export const SecondsCounter = (props) => {
+    return(
+		<div className="counter rounded d-flex justify-content-center">
+			<div className="clock "><i className="fa-regular fa-clock"></i></div>
+			<div className="first ">{props.firstDigit %10}</div>
+			<div className="second">{props.secondDigit %10}</div>
+			<div className="third">{props.thirdDigit %10}</div>
+			<div className="fourth">{props.fourthDigit %10}</div>
+			<div className="fifth ">{props.fifthDigit %10}</div>
+			<div className="sixth">{props.sixthDigit %10}</div>
 		</div>
-	);
-};
+    )
+}
 
 export default Home;
